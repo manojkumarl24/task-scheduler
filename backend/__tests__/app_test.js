0e060f070854621c408ac3c4c97dfbc856a4974a
MANOJ KUMAR. L <manojkumar@ajirasoft.com>
2025-10-06 18:10:26 +0530

    Modified index. js and added app.js to comply with jest

--- backend/__tests__/app_test.js
+++ backend/__tests__/app_test.js
@@ -1,9 +1,10 @@
 import request from "supertest";
-import app from "../src/index.js";  
+import app from "../src/app.js";
 
-describe('GET /test', () => {
-  it('should return 200', async () => {
-    const res = await request(app).get('/test');
+describe("GET /test", () => {
+  it("should return 200", async () => {
+    const res = await request(app).get("/test");
     expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty("message", "OK");
   });
 });
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,35 +1,6 @@
-import express from "express";
-import cors from 'cors';
-import authRoutes from "./routes/authRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-import resourceRoutes from "./routes/resourceRoutes.js";
-import taskRoutes from "./routes/taskRoutes.js";
-import { authenticateJWT } from "./middlewares/authMiddleware.js"; 
+import app from "./app";
 
-const app = express();
 const PORT = process.env.PORT || 2108;
-
-app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173", 
-    credentials: true, 
-  })
-);
-
-app.use("/auth", authRoutes);
-
-app.use("/users", authenticateJWT, userRoutes);
-app.use("/resources", authenticateJWT, resourceRoutes);
-app.use("/tasks", authenticateJWT, taskRoutes);
-
-
-app.get("/me", authenticateJWT, (req, res) => res.json({ user: req.user }));
-
-app.get('/test', (req, res) => {
-  res.json({ status: 'ok' });
-});
-
 
 app.listen(PORT, () => {
   console.log(`API listening on port ${PORT}`);
